eb6edc5a95a943abb2fb639f8889074517083876
ThauanyCelestino <83665046+ThauanyCelestino@users.noreply.github.com>
2023-05-10 10:58:38 -0400

    cmmt

--- JS/calculos.js
+++ JS/calculos.js
@@ -1,6 +1,8 @@
+document.querySelector("#btnSomar").addEventListener("click",somarValores);
+
 function somarValores(){
-    var n1 = parseFloat(numero1.value);
-    var n2 = parseFloat(numero2.value);
+    var n1 = parseFloat(document.querySelector("#numero1").value);
+    var n2 = parseFloat(document.querySelector("#numero2").value);
     var soma = n1+n2;
-    resultado.value = soma;
+    document.querySelector("#resultado").value = soma;
 }
