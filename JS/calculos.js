34517911009dc510622fdef51ba7dec27399f762
ThauanyCelestino <83665046+ThauanyCelestino@users.noreply.github.com>
2023-05-10 16:31:07 -0400

    mm

--- JS/calculos.js
+++ JS/calculos.js
@@ -17,25 +17,38 @@
 // });
 
 
-const btnSomar = document.querySelector("#btnSomar");
+const bSomar = document.querySelector("#btnSomar");
 
-btnSomar.addEventListener("click", () =>{
-    var n1 = parseFloat(document.querySelector("#numero1").value);
-    var n2 = parseFloat(document.querySelector("#numero2").value);
-    var soma = n1+n2;
-    document.querySelector("#resultado").value = soma;
+bSomar.addEventListener("click", () =>{
+    var p1 = parseFloat(document.querySelector("#numero1").value);
+    var p2 = parseFloat(document.querySelector("#numero2").value);
+    somar(p1, p2); 
 });
 
-// function somar (p1, p2) {
-//     var soma = p1 + p2;
-//     document.querySelector("#resultado").value = soma;
+ function somar (p1, p2) {
+     var soma = p1 + p2;
+     document.querySelector("#resultado").value = soma
+ }
 
-// }
 
 document.querySelector("#btnImc").addEventListener("click", function
  (){
-     var n1 = parseFloat(document.querySelector("#pesoI").value);
-     var n2 = parseFloat(document.querySelector("#alturaI").value);
-     var calcular = n1/(n2*n2);
+     var peso = parseFloat(document.querySelector("#pesoI").value);
+     var altura = parseFloat(document.querySelector("#alturaI").value);
+     var calcular = peso/ (altura*altura); 
      document.querySelector("#result").value = calcular;
  });
+
+ const bLimpar = document.querySelector("#btnLimpar");
+
+ bLimpar.addEventListener("click", () =>{
+    document.querySelector("#pesoI").value = '';
+    document.querySelector("#alturaI").value = '';
+    document.querySelector("#result").value = '';
+    document.querySelector("#pesoI").focus();
+ });
+
+
+
+
+ 
