725dde84a2ba31c0007c1f44e65429f5865de0e4
Thauany Celestino <83665046+ThauanyCelestino@users.noreply.github.com>
2023-06-05 17:24:33 -0400

    v

--- JS/churrasco.js
+++ JS/churrasco.js
@@ -1,19 +1,37 @@
 const formulario = document.querySelector("#meuForm");
 const convidados = document.querySelector("#lista-convidados");
+const alunos= JSON.parse(localStorage.getItem("alunos")) || [];
+
+alunos.forEach((elemento)=>{
+console.log(elmento.aluno, elemento.qtde)
+});
+
 formulario.addEventListener("submit", (evento) => {
     evento.preventDefault()
-    novoElemento(evento.target.elements['aluno'].value,
-    evento.target.elements['qtde'].value)
+    const aluno = evento.target.elements['aluno'];
+    const qtde = evento.target.elements['qtde'];
+    novoElemento(aluno.value, qtde.value);
+    aluno.value = "";
+    qtde.value = "";  
+
 });
 
 function novoElemento (aluno, qtde){
     const novoItem = document.createElement('li');
-    novoItem.innerHTML = aluno;
-    novoItem.classList.add('item');
+    novoItem.classList.add ("item");
+    novoItem.innerHTML =  aluno;
 
     const numeroItem = document.createElement('span');
     numeroItem.innerHTML =  qtde;
 
     novoItem.appendChild(numeroItem);
     convidados.appendChild(novoItem);
+
+    const alunoStorage = {
+        "aluno":aluno,
+        "qtde":qtde
+    }
+    alunos.push(alunoStorage)
+    localStorage.setItem("alunos",JSON.stringify(alunos))
 }
+
