d43cc34aa88d964d46c7b85677de347c7e56857b
Thauany Celestino <83665046+ThauanyCelestino@users.noreply.github.com>
2023-06-07 11:52:11 -0400

    cmt

--- JS/churrasco.js
+++ JS/churrasco.js
@@ -3,35 +3,52 @@ const convidados = document.querySelector("#lista-convidados");
 const alunos= JSON.parse(localStorage.getItem("alunos")) || [];
 
 alunos.forEach((elemento)=>{
-console.log(elmento.aluno, elemento.qtde)
+novoElemento(elemento)
 });
 
 formulario.addEventListener("submit", (evento) => {
     evento.preventDefault()
     const aluno = evento.target.elements['aluno'];
     const qtde = evento.target.elements['qtde'];
-    novoElemento(aluno.value, qtde.value);
+    const convidado = alunos.find(elemento => elemento.aluno === aluno.value)
+    const alunoStorage = {
+        "aluno":aluno.value,
+        "qtde":qtde.value
+    }
+    if (convidado) {
+        alunoStorage.id = convidado.id;
+        mudaElemento(alunoStorage);
+        alunos[alunos.find.Index(elemento => elemento.id === convidado.id)] = 
+        alunoStorage;
+
+    } else{
+        alunoStorage.id = alunos[alunos.length - 1] ? (aluno[alunos.length - 1]).id+1
+        :0;
+
+        novoElemento(alunoStorage);
+        alunos.push(alunoStorage);
+    }
+
+    localStorage.setItem("alunos",JSON.stringify(alunos))
     aluno.value = "";
     qtde.value = "";  
 
 });
 
-function novoElemento (aluno, qtde){
-    const novoItem = document.createElement('li');
-    novoItem.classList.add ("item");
-    novoItem.innerHTML =  aluno;
-
-    const numeroItem = document.createElement('span');
-    numeroItem.innerHTML =  qtde;
-
-    novoItem.appendChild(numeroItem);
-    convidados.appendChild(novoItem);
-
-    const alunoStorage = {
-        "aluno":aluno,
-        "qtde":qtde
-    }
-    alunos.push(alunoStorage)
-    localStorage.setItem("alunos",JSON.stringify(alunos))
+function mudaElemento(alunoUpdate) {
+    console.log(document.querySelector("[data-id='"+alunoUpdate.id+"']").
+    innerHTML = alunoUpdate.qtde);
 }
 
+function novoElemento (alunoAdd){
+    const novoItem = document.createElement('li');
+    novoItem.classList.add ("item");
+    novoItem.innerHTML =  alunoAdd.aluno;
+
+    const numeroItem = document.createElement('span');
+    numeroItem.innerHTML =  alunoAdd.qtde;
+    numeroItem.dataset.id = alunoAdd.id;
+    novoItem.appendChild(numeroItem);
+    convidados.appendChild(novoItem);
+  
+}
